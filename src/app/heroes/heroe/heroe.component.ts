4aa00c3e4593af67c55d652ce7cf84ffbbec034d
coazit <coazit@gmail.com>
2021-02-13 18:28:52 -0500

    change funcionalidad heroe

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -5,6 +5,16 @@ import { Component } from "@angular/core";
     templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
-    nombre: string;
+    nombre  : string = 'Ironman';
+    edad    : number = 45;
+
+    get nombreCapitalizado():string {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`;    
+    }
+
 
 }
