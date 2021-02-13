dd7621032f9c330e16c9d75e6fa681072dd4c6de
coazit <coazit@gmail.com>
2021-02-13 17:53:12 -0500

    ContadorComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-
-  acumular(valor: number) {
-    this.numero += valor;
-  }  
+  
 }
