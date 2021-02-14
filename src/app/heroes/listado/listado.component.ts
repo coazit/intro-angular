83f150dbc9bd2a6b579f275ebcfd7f1567091143
coazit <coazit@gmail.com>
2021-02-14 01:09:11 -0500

    ngIf-else

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -10,6 +10,6 @@ export class ListadoComponent {
   heroeBorrado: string = '';
 
   borrarHeroe(): void {
-    this.heroeBorrado = this.heroes.pop() || '';
+    this.heroeBorrado = this.heroes.shift() || '';
   }
 }
